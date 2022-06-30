55e8886ba770435ea0b00bfbd0eb5181493fc918
Somov <somov.studio@gmail.com>
2022-06-30 12:41:23 +0300

    Метод OpenFile в редакторе кода

--- App/Hat/Hat/Editor/CodeEditorForm.cs
+++ App/Hat/Hat/Editor/CodeEditorForm.cs
@@ -22,7 +22,7 @@ namespace Hat
         const string STATUS_EDIT = "status_edit";
 
         public BrowserForm parent;
-        List<string[]> files;
+        List<string[]> files; // [имя файла | путь файла | статус | индекс]
 
         private void CodeEditorForm_Load(object sender, EventArgs e)
         {
@@ -44,8 +44,22 @@ namespace Hat
 
         public void OpenFile(string filename, string path)
         {
+            int count = files.Count;
+            if(count > 0)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    if (files[i][1] == path)
+                    {
+                        parent.consoleMsg($"Файл {filename} уже открыт в редакторе");
+                        return;
+                    }
+                }
+            }
+
+
             int index = tabControl1.TabPages.Count;
-            files.Add(new string[] { filename, path, STATUS_SAVED, index.ToString() });
+            files.Add(new string[] { filename, path, STATUS_SAVED, index.ToString() }); // [имя файла | путь файла | статус | индекс]
             
             WorkOnFiles reader = new WorkOnFiles();
             TextEditorControl textEditorControl = new TextEditorControl();
