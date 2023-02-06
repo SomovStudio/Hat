fb0ec07db98d0401b47712f3b04df630241ec09e
Somov Studio <somov.studio@gmail.com>
2023-02-06 10:02:38 +0300

    Добавлены иконки

--- App/Hat/Hat/Config/Config.cs
+++ App/Hat/Hat/Config/Config.cs
@@ -25,7 +25,7 @@ namespace Hat
         public static BrowserForm browserForm;                  // окно браузера (форма)
         public static string defaultUserAgent = "";             // значение user-agent по умолчанию
         public static string currentBrowserVersion = "1.2.0";   // текущая версия браузера
-        public static string dateBrowserUpdate = "01.02.2023";  // дата последнего обновления
+        public static string dateBrowserUpdate = "07.02.2023";  // дата последнего обновления
 
         public static string openHtmlFile = null;             // имя открываемого html файла при запуске браузера
         public static bool commandLineMode = false;         // флаг показывающий запуск приложения из командной строки
