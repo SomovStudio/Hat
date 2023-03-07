d585a66f445b51502cbe54a04eee0d1dba046ddc
Somov Studio <somov.studio@gmail.com>
2023-03-07 10:18:37 +0300

    Программа для запуска Jenkins агента

---
+++ Jenkins/JenkinsAgentRun/JenkinsAgentRun/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace JenkinsAgentRun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.consoleRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.выключитьАгентаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выключитьАгентаToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выключитьАгентаToolStripMenuItem1});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Jenkins Agent";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // consoleRichTextBox
+            // 
+            this.consoleRichTextBox.BackColor = System.Drawing.Color.White;
+            this.consoleRichTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.consoleRichTextBox.Location = new System.Drawing.Point(0, 24);
+            this.consoleRichTextBox.Name = "consoleRichTextBox";
+            this.consoleRichTextBox.ReadOnly = true;
+            this.consoleRichTextBox.Size = new System.Drawing.Size(784, 515);
+            this.consoleRichTextBox.TabIndex = 2;
+            this.consoleRichTextBox.Text = "";
+            this.consoleRichTextBox.WordWrap = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выключитьАгентаToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(177, 26);
+            // 
+            // выключитьАгентаToolStripMenuItem
+            // 
+            this.выключитьАгентаToolStripMenuItem.Name = "выключитьАгентаToolStripMenuItem";
+            this.выключитьАгентаToolStripMenuItem.Size = new System.Drawing.Size(176, 22);
+            this.выключитьАгентаToolStripMenuItem.Text = "Выключить агента";
+            this.выключитьАгентаToolStripMenuItem.Click += new System.EventHandler(this.выключитьАгентаToolStripMenuItem_Click);
+            // 
+            // выключитьАгентаToolStripMenuItem1
+            // 
+            this.выключитьАгентаToolStripMenuItem1.Name = "выключитьАгентаToolStripMenuItem1";
+            this.выключитьАгентаToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.выключитьАгентаToolStripMenuItem1.Text = "Выключить агента";
+            this.выключитьАгентаToolStripMenuItem1.Click += new System.EventHandler(this.выключитьАгентаToolStripMenuItem1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.consoleRichTextBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Jenkins - Agent";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.RichTextBox consoleRichTextBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem выключитьАгентаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выключитьАгентаToolStripMenuItem1;
+    }
+}
+
---
+++ Jenkins/JenkinsAgentRun/JenkinsAgentRun/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JenkinsAgentRun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public delegate void AddConsoleItem(String message);
+        public AddConsoleItem myDelegate;
+        Process P;
+
+        private bool CancelClose = true;
+
+        public void addConsoleItemMethod(String message)
+        {
+            consoleMessage(message);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myDelegate = new AddConsoleItem(addConsoleItemMethod);
+
+            try
+            {
+                P = new Process();
+                P.StartInfo.FileName = "run-agent.bat";
+                P.StartInfo.Arguments = "/k";
+                P.StartInfo.RedirectStandardError = true;
+                P.StartInfo.RedirectStandardInput = true;
+                P.StartInfo.RedirectStandardOutput = true;
+                P.StartInfo.CreateNoWindow = true;
+                P.StartInfo.UseShellExecute = false;
+                P.ErrorDataReceived += P_ErrorDataReceived;
+                P.OutputDataReceived += P_OutputDataReceived;
+                P.Start();
+                P.BeginErrorReadLine();
+                P.BeginOutputReadLine();
+            }
+            catch (Exception ex)
+            {
+                consoleMessage(ex.Message);
+            }
+        }
+
+        void P_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            try
+            {
+                this.Invoke(this.myDelegate, new object[] { e.Data.ToString() });
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        void P_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            try
+            {
+                this.Invoke(this.myDelegate, new object[] { e.Data.ToString() });
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                if (P != null)
+                {
+                    P.Close();
+                    P.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = CancelClose;
+            if (CancelClose == true) this.Visible = false;
+        }
+
+        private void consoleMessage(String message)
+        {
+            consoleRichTextBox.Text = consoleRichTextBox.Text + message + Environment.NewLine;
+            consoleRichTextBox.Select(consoleRichTextBox.Text.Length, consoleRichTextBox.Text.Length);
+            consoleRichTextBox.ScrollToCaret();
+        }
+
+        private void выключитьАгентаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CancelClose = false;
+            Close();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+        }
+
+        private void выключитьАгентаToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            CancelClose = false;
+            Close();
+        }
+    }
+}
